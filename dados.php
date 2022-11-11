164ad4953683e2f977c5fc9352045f38a9f35fa6
Thiago Bruno <tbgt35@hotmail.com>
2022-11-11 09:28:14 -0300

    first commmit

---
+++ dados.php
@@ -0,0 +1,23 @@
+<?php 
+
+$alunos = [];
+
+$alunos[] = [
+    'nome' => 'Chiquim',
+    'cidade' => 'Caucaia',
+    'matricula' => '23',
+];
+
+$alunos[] = [
+    'nome' => 'Madeinusa',
+    'cidade' => 'Quixada',
+    'matricula' => '28',
+];
+
+$alunos[] = [
+    'nome' => 'Valcigleydson',
+    'cidade' => 'Mauriti',
+    'matricula' => '11'
+];
+
+include 'tabela.php';
